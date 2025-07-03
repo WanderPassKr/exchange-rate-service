26ddb19f1a777169a66f10a01e06b1cc50514b37
glaxyt <glaxyt@naver.com>
2025-07-03 19:13:09 +0900

    feat: 원하는 환율 데이터가 캐시에 존재하지않을 경우 fallback 을 위한 조회 메서드 구현

--- src/main/java/com/wanderpass/exchange_rate_service/exchange/infrastructure/persistence/ExchangeRateRepository.java
+++ src/main/java/com/wanderpass/exchange_rate_service/exchange/infrastructure/persistence/ExchangeRateRepository.java
@@ -1,6 +1,13 @@
 package com.wanderpass.exchange_rate_service.exchange.infrastructure.persistence;
 
 import com.wanderpass.exchange_rate_service.exchange.domain.entity.ExchangeRate;
+import com.wanderpass.exchange_rate_service.exchange.domain.type.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {}
+import java.util.List;
+import java.util.Optional;
+
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
+    Optional<ExchangeRate> findTopByFromCurrencyAndToCurrencyOrderByFetchedAtDesc(Currency from, Currency to);
+}
