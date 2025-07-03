a9275a965ba74ab34e49bd897426f0756f9801e4
glaxyt <glaxyt@naver.com>
2025-07-03 17:07:07 +0900

    refactor: 외부 API 연동 중 환율 처리 예외 패키지 이동

--- src/main/java/com/wanderpass/exchange_rate_service/exchange/exception/exchange/ExchangeRateApiException.java
+++ src/main/java/com/wanderpass/exchange_rate_service/exchange/exception/exchange/ExchangeRateApiException.java
@@ -1,7 +1,7 @@
 package com.wanderpass.exchange_rate_service.exchange.exception.exchange;
 
-import com.wanderpass.exchange_rate_service.exchange.exception.type.IntegrationException;
-import com.wanderpass.exchange_rate_service.exchange.exception.type.StatusCode;
+import com.wanderpass.exchange_rate_service.global.exception.type.IntegrationException;
+import com.wanderpass.exchange_rate_service.global.exception.type.StatusCode;
 
 public class ExchangeRateApiException extends IntegrationException {
     public ExchangeRateApiException(StatusCode statusCode, String message) {
